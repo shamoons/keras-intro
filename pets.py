d1c0f180582a8d8ecf00662d71b46d70f325e7cb
Shamoon Siddiqui <shamoon@develop.io>
2018-12-29 11:37:04 -0500

    Getting 97% accuracy

--- pets.py
+++ pets.py
@@ -4,19 +4,29 @@ import numpy
 import pandas as pd
 
 X = pd.read_csv(
-    "data/train.csv", usecols=['Type', 'Age', 'Breed1', 'Breed2', 'Gender', 'Color1', 'Color2', 'Color3'])
+    "data/train.csv", header=0, usecols=['Type', 'Age', 'Breed1', 'Breed2', 'Gender', 'Color1', 'Color2', 'Color3', 'MaturitySize',	'FurLength',	'Vaccinated',	'Dewormed',	'Sterilized',	'Health',	'Quantity',	'Fee', 'VideoAmt', 'PhotoAmt', 'State'])
 Y = pd.read_csv(
-    "data/train.csv", usecols=['AdoptionSpeed'])
+    "data/train.csv", header=0, usecols=['AdoptionSpeed'])
+
+X = pd.get_dummies(X, columns=["Type", "Breed1",
+                               "Breed2", "Color1", "Color2", "Color3", "Gender", "MaturitySize", "FurLength", "State"])
+print(X)
+
+Y = Y['AdoptionSpeed'].apply(lambda v: v / 4)
+
+input_units = X.shape[1]
 
 model = Sequential()
-model.add(Dense(8, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(input_units, input_dim=input_units, activation='relu'))
+model.add(Dense(input_units, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
-# Compile model
 model.compile(loss='binary_crossentropy',
               optimizer='adam', metrics=['accuracy'])
-# Fit the model
-model.fit(X, Y, epochs=150, batch_size=1000)
-# evaluate the model
+model.fit(X, Y, epochs=250, batch_size=1000)
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+# submission = pd.DataFrame(data={"PetID": X["PetID"],
+#                                 "AdoptionSpeed": Y})
+# submission.AdoptionSpeed = submission.AdoptionSpeed * 4
+# submission.to_csv("submission.csv", index=False)
