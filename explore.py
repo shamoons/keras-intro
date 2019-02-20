bf863560516b6b36751806f069fb5df8f559f44e
Shamoon Siddiqui <shamoon@develop.io>
2019-02-20 15:53:02 -0500

    Tryign to get training to work

--- explore.py
+++ explore.py
@@ -11,45 +11,21 @@ X = X.drop(['Description', 'AdoptionSpeed', 'Name', 'PetID'], axis=1)
 X = pd.get_dummies(X, columns=['Type', 'Breed1', 'Breed2', 'Gender', 'Color1', 'Color2', 'Color3', 'MaturitySize',
                                'FurLength', 'Vaccinated', 'Dewormed', 'Sterilized', 'Health', 'State', 'RescuerID'])
 X['Age'] = X['Age'].apply(lambda v: v / max(X['Age']))
-X['Quantity'] = X['Quantity'].apply(lambda v: v / max(X['Quantity']))
-X['VideoAmt'] = X['VideoAmt'].apply(lambda v: v / max(X['VideoAmt']))
-X['PhotoAmt'] = X['PhotoAmt'].apply(lambda v: v / max(X['PhotoAmt']))
+X['Quantity'] = X['Quantity'] / X['Quantity'].max()
+X['VideoAmt'] = X['VideoAmt'] / X['VideoAmt'].max()
+X['PhotoAmt'] = X['PhotoAmt'] / X['PhotoAmt'].max()
 
 input_units = X.shape[1]
 output_units = Y.shape[1]
-print(output_units)
-# model = Sequential()
-# model.add(Dense(input_units, input_dim=input_units, activation='relu'))
+model = Sequential()
+model.add(Dense(input_units, input_dim=input_units, activation='relu'))
 
-# model.add(Dense(output_units, activation='softmax'))
+model.add(Dense(output_units, activation='softmax'))
 
-# model.compile(loss='binary_crossentropy',
-#               optimizer='adam', metrics=['accuracy'])
-# model.fit(X, Y, epochs=250, batch_size=250,
-#           shuffle=True, validation_split=0.05, verbose=2)
+model.compile(loss='binary_crossentropy',
+              optimizer='adam', metrics=['accuracy'])
+model.fit(X, Y, epochs=250, batch_size=250,
+          shuffle=True, validation_split=0.05, verbose=2)
 
-# scores = model.evaluate(X, Y)
-# print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-
-
-# X = []
-
-# Type
-# pet_type = keras.utils.to_categorical(train['Type'], dtype='int32')
-# X.append(pet_type)
-
-# Age
-# age = train['Age'].apply(lambda v: v / max(train['Age']))
-# print(age)
-# X.append(age)
-
-
-# test = pd.read_csv(datapath + '/test/test.csv')
-# sub = pd.read_csv(datapath + '/test/sample_submission.csv')
-
-# train['dataset_type'] = 'train'
-# test['dataset_type'] = 'test'
-# all_data = pd.concat([train, test])
-# print(train['Type'])
-# print(X)
-# print(Y)
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
