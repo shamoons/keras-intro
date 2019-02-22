f55abb78fdf34e16988e15189d91ee3c576b02dc
Shamoon Siddiqui <shamoon@develop.io>
2019-02-22 10:26:23 -0500

    Save model saver

--- explore.py
+++ explore.py
@@ -5,6 +5,7 @@ from tensorflow.python.client import device_lib
 from dotenv import load_dotenv
 import kappa
 import os
+import time
 load_dotenv()
 
 print(device_lib.list_local_devices())
@@ -19,6 +20,7 @@ X = pd.get_dummies(X, columns=['Type', 'Breed1', 'Breed2', 'Gender', 'Color1', '
                                'FurLength', 'Vaccinated', 'Dewormed', 'Sterilized', 'Health', 'State', 'RescuerID'])
 X['Age'] = X['Age'].apply(lambda v: v / max(X['Age']))
 X['Quantity'] = X['Quantity'] / X['Quantity'].max()
+X['Fee'] = X['Fee'] / X['Fee'].max()
 X['VideoAmt'] = X['VideoAmt'] / X['VideoAmt'].max()
 X['PhotoAmt'] = X['PhotoAmt'] / X['PhotoAmt'].max()
 
@@ -31,8 +33,11 @@ model.add(Dense(output_units, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
               optimizer='adam', metrics=['accuracy'])
-model.fit(X, Y, epochs=50, batch_size=100, shuffle=True,
+model.fit(X, Y, epochs=1, batch_size=100, shuffle=True,
           validation_split=0.05, verbose=2)
 
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+model_name = time.strftime('%Y-%m-%d-%H-%M-%S') + '.h5'
+model.save('models/' + model_name)
