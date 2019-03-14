Fix/output normalization (#3) Score: 0.304
* Trying to get it to work

* Getting annotations saved

* Switched to categorical cross entropy. Score: 0.304

explore.py
@@ -1,8 +1,7 @@
 import keras
 from dotenv import load_dotenv
-
 from keras.callbacks import ModelCheckpoint, CSVLogger
-
+from keras.utils.np_utils import to_categorical
 from sklearn.utils.class_weight import compute_class_weight
 import kappa
 import matplotlib.pyplot as plt
@@ -20,14 +19,8 @@ X = pets.load_input(datapath + '/train.csv', datapath +
                     '/train_sentiment/', keepPetId=False)
 Y = pd.read_csv(datapath + '/train.csv', usecols=['AdoptionSpeed'])
 
-Y = pets.normalize_output(Y)
-
-# def weighted_crossentropy(labels, logits):
-#     return tf.losses.softmax_cross_entropy(
-#         labels,
-#         logits,
-#         weights=tf.abs(tf.argmax(logits, axis=1) - tf.argmax(labels, axis=1)),
-#     )
+# Y = pets.normalize_output(Y)
+Y = to_categorical(Y)
 
 
 input_units = X.shape[1]
@@ -36,12 +29,12 @@ model = pets.get_model(input_size=input_units)
 
 model_name = time.strftime('%Y-%m-%d-%H-%M-%S')
 
-model.compile(loss='mean_squared_error',
+model.compile(loss='categorical_crossentropy',
               optimizer='adam', metrics=['mean_squared_error', 'accuracy'])
 
 csvLogger = CSVLogger('data/' + model_name + '.csv')
 checkpoint = ModelCheckpoint('models/' + model_name + '.h5',
-                             monitor='val_mean_squared_error:', verbose=1, save_best_only=True, save_weights_only=True, mode='auto')
+                             monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=True, mode='auto')
 
 callbacks = [csvLogger, checkpoint]
 history = model.fit(X, Y, epochs=100, shuffle=True, batch_size=512,

pets.py
@@ -9,6 +9,21 @@ import os
 import pandas as pd
 import numpy as np
 
+from dotenv import load_dotenv
+load_dotenv()
+datapath = os.environ['DATA_PATH']
+
+
+def process_image_metadata(X):
+    annotations_file = 'image-annotations.json'
+    json_data = json.loads(open(annotations_file).read())
+
+    vector_size = len(json_data)
+    print(json_data)
+    print(X.shape)
+
+    return X
+
 
 def process_sentiment(X, sentiment_path):
     processedX = X
@@ -35,6 +50,7 @@ def load_input(petfile, sentiment_path, keepPetId=False):
                                    'FurLength', 'Vaccinated', 'Dewormed', 'Sterilized', 'Health', 'State'])
 
     X = process_sentiment(X, sentiment_path)
+    X = process_image_metadata(X)
 
     X = X.drop(['Description', 'AdoptionSpeed', 'Name', 'RescuerID'],
                axis=1, errors='ignore')
@@ -56,17 +72,13 @@ def load_input(petfile, sentiment_path, keepPetId=False):
 
 def normalize_output(Y):
     newY = Y / 4
-    # newY = pd.DataFrame([np.ones(x)
-    #                      for x in Y['AdoptionSpeed']]).fillna(0).astype(int)
 
     return newY
 
 
 def denormalize_output(y):
-    # output_values = np.array(
-    #     [[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 0, 0], [1, 1, 1, 0], [1, 1, 1, 1]])
-    # ret = [np.argmin(np.linalg.norm(output_values-i, axis=1)) for i in y]
-    ret = np.rint(y * 4)
+    ret = (y * 4)
+    ret = ret.astype(int)
     return ret
 
 
@@ -107,6 +119,6 @@ def get_model(input_size):
     model.add(BatchNormalization())
     model.add(Dropout(0.5))
 
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(5, activation='softmax'))
 
     return model